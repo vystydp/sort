b3d124d3cbf370820b76a6cedb8f45e06914bf71
Petr Vystyd <vystydp@gmail.com>
2016-10-09 03:57:29 +0200

    table header

--- es6/index-compiled.js
+++ es6/index-compiled.js
@@ -11,9 +11,31 @@ const heap = new BinaryHeap(item => Math.abs(item.lastChangeBid));
 const queue = new QueueService();
 
 var tableCreate = function (data) {
-  var tbl = document.createElement('table');
+  let tbl = document.createElement('table');
   tbl.id = "currency-dashboard";
-  var tbdy = document.createElement('tbody');
+  let thd = document.createElement('thead');
+  let thdTr = document.createElement('tr');
+  let name = document.createElement('th');
+  let bestBid = document.createElement('th');
+  let bestAsk = document.createElement('th');
+  let openBid = document.createElement('th');
+  let lastChangeBid = document.createElement('th');
+  let sparks = document.createElement('th');
+  name.appendChild(document.createTextNode('name'));
+  bestBid.appendChild(document.createTextNode('bestBid'));
+  bestAsk.appendChild(document.createTextNode('bestAsk'));
+  openBid.appendChild(document.createTextNode('openBid'));
+  lastChangeBid.appendChild(document.createTextNode('lastChangeBid'));
+  sparks.appendChild(document.createTextNode('sparks'));
+  thdTr.appendChild(name);
+  thdTr.appendChild(bestBid);
+  thdTr.appendChild(bestAsk);
+  thdTr.appendChild(openBid);
+  thdTr.appendChild(lastChangeBid);
+  thdTr.appendChild(sparks);
+  thd.appendChild(thdTr);
+  tbl.appendChild(thd);
+  let tbdy = document.createElement('tbody');
   data.forEach(item => {
     let tr = document.createElement('tr');
     let name = document.createElement('td');
@@ -24,10 +46,10 @@ var tableCreate = function (data) {
 
     let sparkline = document.createElement('td');
     var el = document.createElement('span');
-    var sparkline1 = new Sparkline(sparkline);
+    var sparkline1 = new Sparkline(sparkline, { 'width': 200 });
     sparkline1.draw(item.midPrice);
     if (item.name == "gbpjpy") {
-      console.log(item.midPrice.length);
+      console.log(item.midPrice);
     }
     name.appendChild(document.createTextNode(item.name));
     bestBid.appendChild(document.createTextNode(item.bestBid));
@@ -58,25 +80,6 @@ window.setInterval(() => {
   var tableEl = document.getElementById('currency-dashboard');
   let parentEl = tableEl.parentNode;
   parentEl.replaceChild(tableCreate(data), tableEl);
-  /*
-  var table = "<table>content</table>";
-  table += "";
-  var mask = TABLE_HEAD_HTML;
-  mask += "<tbody>";
-  data.forEach((item) => {
-    mask += '' +
-      '<tr>' +
-        ' <td>' + item.name + '</td> ' +
-        ' <td>' + item.bestBid + '</td> ' +
-        ' <td>' + item.bestAsk + '</td> ' +
-        ' <td>' + item.openBid + '</td> ' +
-        ' <td>' + item.lastChangeBid + '</td> ' +
-        '<td id="sparks-"+item.name>&nbsp</td>' +
-      '</tr>';
-  });
-  mask += "</tbody>";
-  appEl.innerHTML = table.replace('content', mask);
-  */
 }, DELAY);
 
 //# sourceMappingURL=index-compiled.js.map
